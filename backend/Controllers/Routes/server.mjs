200878713f493c16bab1f5f73bac4faea1f31022
José Vitor Vegini <josevegini12@gmail.com>
2024-08-24 16:03:13 -0300

    backend assunto

--- backend/Controllers/Routes/server.mjs
+++ backend/Controllers/Routes/server.mjs
@@ -118,6 +118,23 @@ app.post('/api/criar-sala', async (req, res) => {
   }
 });
 
+app.post('/api/cadastrar-assunto', async (req, res) => {
+  const { nome, descricao, monitor_id } = req.body;
+
+  try {
+      await pool.query(
+          'INSERT INTO disciplinas (nome, descricao, monitor_id) VALUES (?, ?, ?)',
+          [nome, descricao, monitor_id]
+      );
+
+      res.status(200).json({ success: true, message: 'Assunto cadastrado com sucesso!' });
+  } catch (error) {
+      console.error('Erro ao cadastrar assunto:', error);
+      res.status(500).json({ success: false, message: 'Erro ao cadastrar assunto' });
+  }
+});
+
+
 
 // Inicializar o servidor
 app.listen(port, () => {
