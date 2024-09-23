3506adb8c3f6db0f92b6776ec1deea6cb166b1b8
José Vitor Vegini <josevegini12@gmail.com>
2024-09-23 19:27:24 -0300

    mudanças rota do monitor

--- backend/Controllers/Routes/userRoutes.mjs
+++ backend/Controllers/Routes/userRoutes.mjs
@@ -19,11 +19,19 @@ router.post('/create-user', async (req, res) => {
     const senhaHash = await bcrypt.hash(senha, 10);
     await pool.query('INSERT INTO usuarios (nome, email, senha, papel) VALUES (?, ?, ?, ?)', [nome, email, senhaHash, papel]);
 
-    res.status(200).json({ success: true, message: 'Usuário criado com sucesso' });
+    res.status(201).json({ success: true, message: 'Usuário criado com sucesso' });
   } catch (error) {
     console.error('Erro:', error);
     res.status(500).json({ success: false, message: 'Erro ao criar o usuário' });
   }
+
+  try {
+    const [rows, fields] = await pool.query('SELECT 1');
+    console.log('Conexão bem-sucedida!', rows);
+} catch (error) {
+    console.error('Erro ao conectar ao banco:', error);
+}
+
 });
 
 export default router;
